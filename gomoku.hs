c0e21144291b6211b7bf070bab9dca2899f1c5e3
Łukasz Szcześniak <say.hello@viters.net>
2017-05-28 03:43:36 +0200

    Add displaying board

--- gomoku.hs
+++ gomoku.hs
@@ -1,16 +1,22 @@
 import Data.Maybe
 
 data Field = O | X deriving (Eq, Show)
-data Position = Maybe Field
-data Board = Matrix [[Position]]
+data Row = Row [Maybe Field]
+data Board = Board [Row]
 
-showPosition :: Maybe Position -> String
-showPosition (Just a) = show a
-showPosition (Nothing) = "_"
+showPosition :: Maybe Field -> String
+showPosition a = case a of
+                    Nothing -> "-"
+                    Just a -> show a
 
--- instance Show Board where
---     show (Matrix []) = ""
-    -- show (Matrix a) = show a
+instance Show Row where
+    show (Row a) = foldl (\x y -> x ++ showPosition y ++ " ") "" a
+
+instance Show Board where
+    show (Board a) = foldl (\x y -> x ++ show y ++ "\n") "" a
+
+emptyRow :: Row
+emptyRow = Row (replicate 19 Nothing)
 
 emptyBoard :: Board
-emptyBoard = Matrix (replicate 19 $ replicate 19 Nothing)
+emptyBoard = Board (replicate 19 $ emptyRow)
