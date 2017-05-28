d14810b5192c85cc4fd2213cfdb2868e1a948641
Łukasz Szcześniak <say.hello@viters.net>
2017-05-28 06:51:53 +0200

    Add writing on board

--- gomoku/src/Board.hs
+++ gomoku/src/Board.hs
@@ -1,28 +1,33 @@
 module Board where
 
 import Data.Maybe
-
+import Control.Lens
 
 type Position = (Int, Int)
 
 data Field = O | X deriving (Eq, Show)
-data Col = Col [Maybe Field]
-data Board = Board [Col]
+data Row = Row { _columns :: [Maybe Field] }
+data Board = Board { _rows :: [Row] }
 
 showPosition :: Maybe Field -> String
 showPosition a = case a of
                     Nothing -> "-"
                     Just a -> show a
 
-instance Show Col where
-    show (Col a) = foldl (\x y -> x ++ showPosition y ++ " ") "" a
+instance Show Row where
+    show (Row a) = foldl (\x y -> x ++ showPosition y ++ " ") "" a
 
 instance Show Board where
     show (Board a) = foldl (\x y -> x ++ show y ++ "\n") "" a
 
-emptyCol :: Col
-emptyCol = Col (replicate 19 Nothing)
+emptyRow :: Row
+emptyRow = Row (replicate 19 Nothing)
 
 emptyBoard :: Board
-emptyBoard = Board (replicate 19 $ emptyCol)
+emptyBoard = Board (replicate 19 $ emptyRow)
 
+setInRow :: Row -> Int -> Field -> Row
+setInRow (Row fields) y f = Row $ (element y .~ Just f) fields
+
+putOnBoard :: Board -> Position -> Field -> Board
+putOnBoard (Board rows) (x, y) f = Board $ (element x .~ setInRow (rows !! x) y f) rows
