3de8cda1e258cee15779a71b1021b985175c0370
Łukasz Szcześniak <say.hello@viters.net>
2017-06-04 16:30:26 +0200

    First version of game loop

--- gomoku/src/Main.hs
+++ gomoku/src/Main.hs
@@ -1,9 +1,28 @@
 module Main where
 
 import World
+import Control.Monad
+import Text.Read
 
 main :: IO ()
 main = do
-    world <- braveNewWorld
-    -- let world = makeMove world (1, 0)
+    let world = braveNewWorld
+    loop world
 
+loop :: World -> IO ()
+loop currentWorld@(World board currentPlayer) = do
+    putStrLn $ show board
+
+    putStrLn "Write x and y separated with a newline"
+    x <- getCoord
+    y <- getCoord
+    let move = (x, y)
+
+    unless (2 < 1) (loop $ registerMove currentWorld move)
+
+getCoord :: IO Int
+getCoord = do
+    line <- getLine
+    case readMaybe line of 
+        Just a -> return a
+        Nothing -> putStrLn "Invalid input, try again" >> getCoord
