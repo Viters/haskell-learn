1f1d67ebddc040ff27f83791a7f349428ff5067b
Viters <viters266@gmail.com>
2017-06-07 00:03:20 +0200

    Working on game end condition

--- gomoku/src/World.hs
+++ gomoku/src/World.hs
@@ -14,3 +14,24 @@ nextPlayer a = case a of
 
 registerMove :: World -> Position -> World
 registerMove (World board cp) pos = World (putOnBoard board pos cp) (nextPlayer cp)
+
+-- gameEnded :: Board -> Boolean
+-- gameEnded = 
+
+fiveInRow :: Position -> [Position]
+fiveInRow (a, b) = zip [a-2 .. a+2] $ repeat b
+
+fiveInCol :: Position -> [Position]
+fiveInCol (a, b) = zip [a-2 .. a+2] $ repeat b
+
+fiveInDiagLeftRight :: Position -> [Position]
+fiveInDiagLeftRight (a, b) = zip [a-2 .. a+2] [b-2 .. b+2]
+
+fiveInDiagRightLeft :: Position -> [Position]
+fiveInDiagRightLeft (a, b) = zip [a-2 .. a+2] [b+2 .. b-2]
+
+positionsToFields :: Board -> [Position] -> [Maybe Player]
+positionsToFields board = map $ getField board
+
+allTheSame :: (Eq a) => [a] -> Bool
+allTheSame xs = and $ map (== head xs) (tail xs)
